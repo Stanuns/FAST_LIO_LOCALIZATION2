b1e5ec994abee529a4ecb21f5038b2b6f9225219
sunwei-ksdy-robot <sunweijm.sun@gmail.com>
2025-11-21 13:07:48 +0800

    sim to real

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
 
     # Declare arguments
     declare_use_sim_time_cmd = DeclareLaunchArgument(
-        "use_sim_time", default_value="true", description="Use simulation (Gazebo) clock if true"
+        "use_sim_time", default_value="false", description="Use simulation (Gazebo) clock if true"
     )
     declare_config_path_cmd = DeclareLaunchArgument(
         "config_path", default_value=default_config_path, description="Yaml config file path"
